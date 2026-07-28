b6c4464bc7416310b08e7e884133ba1424fa59c8
rem1xzov <b.programc@gmail.com>
2026-07-28 21:31:49 +0300

    Delete alter_images_columns.sql

--- alter_images_columns.sql
+++
@@ -1,3 +0,0 @@
--- Изменяем тип колонки Images на text, чтобы хранить JSON-строку
-ALTER TABLE "ForumThreads" ALTER COLUMN "Images" TYPE text;
-ALTER TABLE "ForumPosts"   ALTER COLUMN "Images" TYPE text;
